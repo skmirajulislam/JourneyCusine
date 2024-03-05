8e5b484d964e555c1ec6aa57d1144da4a18d6b59
Aishiki <126267750+AishDal@users.noreply.github.com>
2024-03-05 10:55:52 +0530

    Update vite.config.js
    Issue by Aishiki on Vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+// Written by Aishiki
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
